4ee40830194a42fac0e018ae75465bf371b0de97
Jordan Delbar <jordandelbar@gmail.com>
2024-11-09 14:02:55 +0100

    Refactor arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,9 @@
 package arrays
 
-func Sum(array [5]int) int {
-	var sum int
-
-	for i := 0; i < len(array); i++ {
-		sum += array[i]
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum

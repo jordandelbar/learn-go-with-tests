9b84045836136a7d498445bdf606bf116eeb357b
Jordan Delbar <jordandelbar@gmail.com>
2024-11-10 17:21:46 +0100

    Add custom type

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,15 +2,25 @@ package wallet
 
 import "fmt"
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }

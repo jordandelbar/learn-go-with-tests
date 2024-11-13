3ba18aa84e8f5fca2cad79e95de53f1dd36294fd
Jordan Delbar <jordandelbar@gmail.com>
2024-11-13 17:18:32 +0100

    Add configurable sleeper

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 const (
@@ -23,6 +24,14 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	return
 }
 
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 func TestCountdown(t *testing.T) {
 	spySleepPrinter := &SpyCountdownOperations{}
 	Countdown(spySleepPrinter, spySleepPrinter)
@@ -39,5 +48,17 @@ func TestCountdown(t *testing.T) {
 
 	if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
 		t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+	}
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 }

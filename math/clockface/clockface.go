a9a42b9a26314fc0eceaf19ffea0fe1f9d2cb1dc
Jordan Delbar <jordan.delbar@exotec.com>
2024-12-17 16:04:08 +0100

    End math

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -5,31 +5,39 @@ import (
 	"time"
 )
 
-const secondHandLength = 90
-const clockCentreX = 150
-const clockCentreY = 150
-
+// A Point represents a two-dimensional Cartesian coordinate.
 type Point struct {
 	X float64
 	Y float64
 }
 
-// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
-// represented as a Point.
-func SecondHand(t time.Time) Point {
-	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + 150, p.Y + 150}
-	return p
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / (float64(t.Second())))
 }
 
-func secondsInRadians(t time.Time) float64 {
-	return (math.Pi / (30 / (float64(t.Second()))))
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) +
+		(math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
